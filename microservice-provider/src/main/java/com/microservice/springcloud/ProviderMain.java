d74c3fbe924fdb51075120b41e7fa3dbadd7ee00
xiaoxiongxiong <mxqxxni@sina.com>
2020-02-05 01:03:13 +0800

    1.修改springcloud,springboot版本 2.添加eureka模块 3.添加provider注册进eureka配置 4.provider注册进eureka显示信息修改

--- microservice-provider/src/main/java/com/microservice/springcloud/ProviderMain.java
+++ microservice-provider/src/main/java/com/microservice/springcloud/ProviderMain.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProviderMain {
     public static final Logger logger = LoggerFactory.getLogger(ProviderMain.class);
     public static void main(String[] args) {
